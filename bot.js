0a045f55b2a03f96ebcfe9bf60ea091901478a61
KIRATI <3673118@ppti-14-503-05.ufr-info-p6.jussieu.fr>
2017-03-29 10:09:52 +0200

    first

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong');
   }
+  else if(message.mentions.users.get(client.user.id)){
+    message.reply('hehe');
+  }
   else if(!message.author.bot){
     message.reply('Hi');
   }
@@ -18,7 +21,6 @@ client.on('message', message => {
 
 client.on('presenceUpdate', function(oldMember, newMember) {
   console.log(oldMember.presence, '=>', newMember.presence);
-  
 });
 
 client.login(process.env.DISCORD_TOKEN);
