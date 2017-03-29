826edbd508048f8ad53315166d3730c4a61aee8b
KIRATI <3673118@ppti-14-503-05.ufr-info-p6.jussieu.fr>
2017-03-29 11:29:34 +0200

    first

--- bot.js
+++ bot.js
@@ -30,3 +30,12 @@ client.on('presenceUpdate', function(oldMember, newMember) {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT || 5000);
